e39fd4524ca4df3bb2866aeeeccb7a60f1c4eb2c
Stephen Jonany <sjonany@gmail.com>
2018-02-17 13:53:43 -0800

    Add routing and ejs

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var app = express();
+const dotenv = require('dotenv');
+const express = require('express');
 
-app.get('/', function(req, res) {
-	res.send("Hello World!");
-});
 
-app.set("port", process.env.PORT || 3000);
-app.listen(app.get("port"), function() {
-	console.log("App is accessible at http://localhost:%d in env %s", app.get("port"), app.get("env"));
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: '.env.example' });
+
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+
+require('./app/routes.js')(app);
+
+app.set('port', process.env.PORT || 3000); 
+app.listen(app.get('port'), function() {
+	console.log('App is accessible at http://localhost:%d in env %s',
+    app.get('port'), app.get('env'));
 });
 
 module.exports = app;
