fe8575f1afb61f01165003a8f1189f771dd9dcb3
Stephen Jonany <sjonany@gmail.com>
2018-02-17 14:31:13 -0800

    Added bootstrap and font-awesome as deps

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 module.exports = function(app) {
 	app.get('/', function(req, res) {
     res.render('index.ejs');
@@ -6,4 +8,8 @@ module.exports = function(app) {
   app.get('/login', function(req, res) {
     res.render('login.ejs');
   });
+
+  // Static scripts to be included in templates.
+  app.use('/dist/bootstrap', express.static(__dirname + '/../node_modules/bootstrap/dist/'));
+  app.use('/dist/font-awesome', express.static(__dirname + '/../node_modules/font-awesome/'));
 }
